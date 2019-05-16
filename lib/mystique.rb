258ab1e7df7497055ba8b772bda4d90f7162504a
Federico Iachetti <iachetti.federico@gmail.com>
2019-05-16 16:15:10 -0300

    Made present and present collection class methods

--- lib/mystique.rb
+++ lib/mystique.rb
@@ -6,9 +6,7 @@ require "mystique/undefined"
 require "mystique/presenter"
 
 module Mystique
-  module_function
-
-  def present(object, with: nil, context: nil, &block)
+  def self.present(object, with: nil, context: nil, &block)
     begin
       presenter_class = presenter_class_for(object, with)
       presenter_class.for(object, context, &block)
@@ -17,7 +15,7 @@ module Mystique
     end
   end
 
-  def present_collection(collection, context: nil, with: nil, &block)
+  def self.present_collection(collection, context: nil, with: nil, &block)
     return to_enum(:present_collection, collection, context: context, with: with, &block) unless block_given?
 
     collection.each do |element|
@@ -30,8 +28,9 @@ module Mystique
     end
   end
 
-  def presenter_class_for(object, with)
+  private
+
+  def self.presenter_class_for(object, with)
     with || Object.send(:const_get, "#{object.class}Presenter")
   end
-  private :presenter_class_for
 end
